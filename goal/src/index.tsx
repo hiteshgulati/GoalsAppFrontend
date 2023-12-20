da9688e1bea61f51a752b2ac4ee094ea85e45e72
Utkarsh <utkarsh.harsh2002@gmail.com>
2023-12-21 00:06:23 +0530

    1.) Added functinality to download 2.) Changed the logos

--- goal/src/index.tsx
+++ goal/src/index.tsx
@@ -5,6 +5,7 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthContextProvider } from './Context/Auth';
 
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
